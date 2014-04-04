c98149b4894bb2652eadb640da3ccf547eeca89e
Jeremy Phelps <jeremy.phelps769@topper.wku.edu>
2014-04-04 15:38:59 -0500

    introduced some templating in place of static html

--- app/views.py
+++ app/views.py
@@ -1,18 +1,12 @@
-from app import app
+from flask import render_template
+from app import app 
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = { 'nickname': 'JP' }
-    return '''
-<html>
-  <head>
-    <title>Home Page</title>
-  </head>
-  <body>
-    <h1>Hello, ''' + user['nickname'] + '''</h1>
-  </body>
-</html>
-'''
+    return render_template("index.html",
+        title = 'Home',
+        user = user)
 
