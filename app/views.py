1050ab4e2ca392fd20273854e56d6d7adc614c7e
Jeremy Phelps <jeremy.phelps769@topper.wku.edu>
2014-04-04 16:40:02 -0500

    added some posts to the index view and template

--- app/views.py
+++ app/views.py
@@ -6,7 +6,19 @@ from app import app
 @app.route('/index')
 def index():
     user = { 'nickname': 'JP' }
+    posts = [
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in BG!'
+        },
+        {
+            'author':{ 'nickname': 'Susan' },
+            'body': 'The movie was good!'
+        }
+    ]
+
     return render_template("index.html",
         title = 'Home',
+        posts = posts,
         user = user)
 
