0bf2d6bf2e36afe42130ada885083f2b5ebd374b
Jeremy Phelps <jeremy.phelps769@topper.wku.edu>
2014-04-08 14:05:19 -0500

    ignore tmp directory and enabled login via openid for index view

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ CSRF_ENABLED = True
 SECRET_KEY = '34rKHEyi__756^%4hglwtjgnosiet-jflHOY!EH&7986%T9y8*7Ljl'
 
 OPENID_PROVIDERS = [
-    { 'name': 'Google', 'url': 'https://www.google.com/accounts/08/id'},
+    { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
