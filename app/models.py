e203e63f50346520fe999bdc1d476b902493f329
Jeremy Phelps <jeremy.phelps769@topper.wku.edu>
2014-04-10 15:10:48 -0500

    added follower association table and migrated database

--- app/models.py
+++ app/models.py
@@ -5,6 +5,13 @@ from hashlib import md5
 ROLE_USER = 0
 ROLE_ADMIN = 1
 
+
+###association table for User model
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), index = True, unique = True)
@@ -13,6 +20,12 @@ class User(db.Model):
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+        secondary = followers,
+        primaryjoin = (followers.c.follower_id == id),
+        secondaryjoin = (followers.c.followed_id == id),
+        backref = db.backref('followers', lazy = 'dynamic'),
+        lazy = 'dynamic')
 
     def is_authenticated(self):
         return True
